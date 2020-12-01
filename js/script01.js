78b9b751bff946911cc698173861fb0ac9d6f55b
Francisco <francisco.dominguez.munoz@iesvegademijas.es>
2020-12-01 14:25:06 +0100

    empezar a hacer la primera mejora

--- js/script01.js
+++ js/script01.js
@@ -1,22 +1,85 @@
-window.onload = initAll;
-let usedNums = new Array(76);
+let tabla = 
+`<table>
+        <tr>
+            <th>B</th>
+            <th>I</th>
+            <th>N</th>
+            <th>G</th>
+            <th>O</th>
+        </tr>
+        <tr>
+            <td id="square0">&nbsp;</td>
+            <td id="square5">&nbsp;</td>
+            <td id="square10">&nbsp;</td>
+            <td id="square14">&nbsp;</td>
+            <td id="square19">&nbsp;</td>
+        </tr>
+        <tr>
+            <td id="square1">&nbsp;</td>
+            <td id="square6">&nbsp;</td>
+            <td id="square11">&nbsp;</td>
+            <td id="square15">&nbsp;</td>
+            <td id="square20">&nbsp;</td>
+        </tr>
+        <tr>
+            <td id="square2">&nbsp;</td>
+            <td id="square7">&nbsp;</td>
+            <td id="free">Free</td>
+            <td id="square16">&nbsp;</td>
+            <td id="square21">&nbsp;</td>
+        </tr>
+        <tr>
+            <td id="square3">&nbsp;</td>
+            <td id="square8">&nbsp;</td>
+            <td id="square12">&nbsp;</td>
+            <td id="square17">&nbsp;</td>
+            <td id="square22">&nbsp;</td>
+        </tr>
+        <tr>
+            <td id="square4">&nbsp;</td>
+            <td id="square9">&nbsp;</td>
+            <td id="square13">&nbsp;</td>
+            <td id="square18">&nbsp;</td>
+            <td id="square23">&nbsp;</td>
+        </tr>
+</table>`;
 
-function initAll() {
+let numCartones = prompt("¿Cuantos cartones quieres?",1);
+
+window.onload = initAll(numCartones);
+let usedNums = new Array(numCartones);
+
+for (let i = 0; i < usedNums.length; i++){
+    usedNums[i] = new Array(76);
+}
+
+function initAll(numCartones) {
     if (document.getElementById) {
-        document.getElementById("reload").onclick = anotherCard;
-        newCard();
+        document.getElementById("reload").onclick = anotherCard(numCartones);
+
+        for (let i = 0; i < numCartones; i++) {
+            newCard(i);    
+        }
     } else {
         alert("Sorry, your browser doesn't support this script");
     }
 }
 
-function newCard() {
+function newCard(thisCarton) {
+    let lienzo = document.createElement("div");
+    lienzo.innerHTML() = tabla;
+    document.body.appendChild(lienzo);
+    
+    console.log(lienzo);
+
     for (let i = 0; i < 24; i++) {
-        setSquare(i);    
+
+        setSquare(thisCarton,i);    
     }
 }
 
-function setSquare(thisSquare) {
+
+function setSquare(thisCarton,thisSquare) {
     let currSquare = "square" + thisSquare;
     let colPlace = new Array(0,0,0,0,0,
                              1,1,1,1,1,
@@ -28,9 +91,11 @@ function setSquare(thisSquare) {
     
     do {
         newNum = colBases + getNewNum() + 1;
-    } while (usedNums[newNum]);
+    } while (usedNums[thisCarton][newNum]);
 
-    usedNums[newNum] = true;
+    usedNums[thisCarton][newNum] = true;
+    document.getElementById(currSquare).id = "square_" + thisCarton + "_" + thisSquare;
+    currSquare = "square_" + thisCarton + "_" + thisSquare;
     document.getElementById(currSquare).innerHTML = newNum;
     document.getElementById(currSquare).className = "";
     document.getElementById(currSquare).onmousedown = toggleColor;
@@ -40,12 +105,14 @@ function getNewNum() {
     return Math.floor(Math.random()*15);
 }
 
-function anotherCard() {
+function anotherCard(numCartones) {
     for (let i = 1; i < usedNums.length; i++){
         usedNums[i] = false;
     }
 
-    newCard();
+    for(let i = 0; i < numCartones; i++) {
+        newCard(i);
+    }
     return false;
 }
 
@@ -76,13 +143,16 @@ function checkWin() {
  *  ...
  *  SOLUCION MAS ALTA POSIBLE ES ->                 11111 00000 0000 00000 00000 = 16252928
  */
-    for(let i = 0; i < 24; i++) {
-        var currSquare = "square" + i;
+    for (let j = 0; j < numCartones; j++) {
 
-        if(document.getElementById(currSquare).className != "") {
-            document.getElementById(currSquare).className = "pickedBG";
-            setSquares = setSquares | Math.pow(2,i);    // PREGUNTAR, CONVIERTE SETSQUARES EN BINARIO Y MATH.POW(2,i) Y LOS UNE CON UN OR -> 00001 | 00010 = 00011
-                                                        // PORQ SETSQUARES = 0 | 2^0 = 1 PARA EL PRIMER CUADRADO, 1 | 2 = 0011
+        for(let i = 0; i < 24; i++) {
+            var currSquare = "square_" + j + "_" + i;
+    
+            if(document.getElementById(currSquare).className != "") {
+                document.getElementById(currSquare).className = "pickedBG";
+                setSquares = setSquares | Math.pow(2,i);    // PREGUNTAR, CONVIERTE SETSQUARES EN BINARIO Y MATH.POW(2,i) Y LOS UNE CON UN OR -> 00001 | 00010 = 00011
+                                                            // PORQ SETSQUARES = 0 | 2^0 = 1 PARA EL PRIMER CUADRADO, 1 | 2 = 0011
+            }
         }
     }
 
@@ -93,10 +163,12 @@ function checkWin() {
     }
 
     if (winningOption > -1) {
-        for (let i=0; i<24; i++){
-            if (winners[winningOption] & Math.pow(2,i)){
-                currSquare = "square" + i;
-                document.getElementById(currSquare).className = "winningBG";
+        for(let j = 0; j < numCartones; j++) {
+            for (let i=0; i<24; i++){
+                if (winners[winningOption] & Math.pow(2,i)){
+                    currSquare = "square_" + j + "_" + i;
+                    document.getElementById(currSquare).className = "winningBG";
+                }
             }
         }
     }
